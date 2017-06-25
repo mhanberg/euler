fb4d31168d5d98d3feba4d7994cc400e68daea9a
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-06-25 01:07:08 -0400

    ruby -- problem 6

---
+++ ruby/problem_6.rb
@@ -0,0 +1,13 @@
+require_relative 'euler_helper'
+start = Time.now
+################
+
+sum_of_the_squares = (1..100).sum { |i| i**2 }
+square_of_the_sum = ((1..100).sum)**2
+answer = square_of_the_sum - sum_of_the_squares
+
+#################
+finish = Time.now
+
+EulerHelper.print_duration(start: start, finish: finish)
+EulerHelper.print_answer answer
